76d79e860bc5a0f51fca1710b8b879154583cb3f
Kevin <vargaskevv@gmail.com>
2022-10-04 21:20:27 -0400

    PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
+const manifest: Partial<ManifestOptions> = {
+  name: 'Rigel',
+  short_name: 'Rigel',
+  description: 'La WebApp de los profesores de Bolivia.',
+  lang: 'es',
+  start_url: '/',
+  display: 'standalone',
+  theme_color: '#0894b3',
+  icons: [
+    {
+      src: '/logo.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+    {
+      src: 'logo.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any mascable',
+    },
+  ],
+};
+
 const pwaConfig = VitePWA({
-  registerType: 'prompt',
+  registerType: 'autoUpdate',
   strategies: 'injectManifest',
   srcDir: 'src',
   filename: 'sw.ts',
@@ -13,6 +36,7 @@ const pwaConfig = VitePWA({
     enabled: true,
     type: 'module',
   },
+  manifest,
 });
 
 export default defineConfig({
