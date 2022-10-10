a63e3006ff0ba8919fb39bc7ae2000f24bc416df
Kevin <vargaskevv@gmail.com>
2022-10-10 12:52:49 -0400

    Fix scores color closes #31

--- src/components/ColoredScore.tsx
+++ src/components/ColoredScore.tsx
@@ -8,20 +8,25 @@ const ColoredScore: Component<{ score: number } & TextProps> = (props) => {
 
   const getColor = createMemo(() => {
     switch (true) {
-      case props.score < 51:
-        return '$danger9';
-      case props.score < 64:
-        return '$warning10';
-      case props.score < 80:
-        return '$info10';
+      case props.score <= 50:
+        return '$danger11';
+      case props.score <= 60:
+        return '#d0b322';
+      case props.score <= 70:
+        return '#b1bd12';
+      case props.score <= 80:
+        return '#62a901';
+      case props.score <= 90:
+        return '#24a401';
       case props.score <= 100:
-        return '$success10';
+        return '#00a016';
       default:
         return '$neutral10';
     }
   });
+
   return (
-    <Text color={coloredScores() ? getColor() : '$neutral12'} {...props} fontWeight="$semibold" size="base">
+    <Text color={coloredScores() ? getColor() : '$neutral12'} fontWeight="$medium" size="base" {...props}>
       {props.score}
     </Text>
   );
