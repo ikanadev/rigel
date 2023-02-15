ac0868ec60e1c24f9ba0391164009b9cf4954a8a
Kevin <vargaskevv@gmail.com>
2023-02-14 23:55:08 -0400

    Change anchor for link in activities screen

--- src/pages/Activities/Area.tsx
+++ src/pages/Activities/Area.tsx
@@ -1,6 +1,7 @@
 import { Component, For, Show } from 'solid-js';
 import { Activity, Area as IArea } from '@app/types';
-import { Heading, Text, Box, Flex, Anchor, IconButton } from '@hope-ui/solid';
+import { Heading, Text, Box, Flex, IconButton } from '@hope-ui/solid';
+import { Link } from '@solidjs/router';
 import { ArrowLongRight, Pencil } from '@app/icons';
 
 import { db } from '@app/db/dexie';
@@ -29,12 +30,12 @@ const Area: Component<Props> = (props) => {
       >
         <For each={activities}>{(activity) => (
           <Flex alignItems="center" p="$1" mt="$1" rounded="$sm" _hover={{ background: '$neutral2' }}>
-            <Anchor href={`activity/${activity.id}`}>
+            <Link href={`/class/${classStore.class!.id}/activity/${activity.id}`}>
               <Flex alignItems="center" color="$primary10">
                 <ArrowLongRight mr="$2" />
                 <Text as="span">{activity.name}</Text>
               </Flex>
-            </Anchor>
+            </Link>
             <Flex flex="1" />
             <IconButton
               onClick={[props.onEditActivity, activity]}
